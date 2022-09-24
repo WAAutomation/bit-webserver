06322496ee97f3c510f6e34961ae22be76a19d06
JiaweiYe <1241818274@qq.com>
2022-09-24 16:13:40 +0800

    static response test

--- WebServer.py
+++ WebServer.py
@@ -20,14 +20,14 @@ class WebServer:
         # 等待新客户端的连接
         while True:
             new_socket, client_addr = self.server_socket.accept()
-            self.response_test(new_socket)
+            self.response_test2(new_socket)
 
     def close(self):
         """关闭服务器连接"""
         self.server_socket.close()
 
     @staticmethod
-    def response_test(new_socket):
+    def response_test1(new_socket):
         """向客户端返回数据（测试用）"""
         # 1.接收浏览器发送过来的请求，即HTTP请求
         # GET / HTTP/1.1
@@ -44,6 +44,34 @@ class WebServer:
 
         new_socket.close()
 
+    @staticmethod
+    def response_test2(new_socket):
+        request = new_socket.recv(1024)
+        print(request)
+        file_name1 = "/error.html"
+        file_name2 = "/style.css"
+        try:
+            #  准备发送的body，打开HTML文件
+            f1 = open("page" + file_name1, 'rb')
+            f2 = open("page" + file_name2, 'rb')
+        except:
+            response = "HTTP/1.1 404 NOT FOUND\r\n"
+            response += '\r\n'
+            response += "----file not found----"
+            new_socket.send(response.encode("utf-8"))
+        else:
+            html_content = f1.read()
+            # html_content += f2.read()
+            f1.close()
+            f2.close()
+            #  准备发送的header
+            response = "HTTP/1.1 200 OK\r\n"
+            response += "\r\n"  # header与body之间必须隔一行
+            #  发送header
+            new_socket.send(response.encode("utf-8"))
+            #  发送HTML
+            new_socket.send(html_content)
+        new_socket.close()
 
 if __name__ == '__main__':
     web_server = WebServer("127.0.0.1", 8888, 128)
