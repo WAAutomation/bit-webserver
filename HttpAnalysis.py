928b5ae5758ddf97cd61bdc677eec2c6898f639b
JiaweiYe <1241818274@qq.com>
2022-09-25 16:14:15 +0800

    add HttpAnalysis

--- HttpAnalysis.py
+++ HttpAnalysis.py
@@ -1,4 +1,4 @@
-class HttpHeaderAnalysis:
+class HttpAnalysis:
     def __init__(self):
         # 请求行
         self.method = None          # GET/POST
@@ -13,9 +13,25 @@ class HttpHeaderAnalysis:
         self.response_line = ''
         self.response_head = dict()  # 请求头
         self.response_body = ''  # 最终请求的html代码
-        self.session = None
 
-    def passRequestLine(self, request_line):  # 定义method/url/protocol
+    def request_analyse(self, request):
+        """请求报文解析入口"""
+        request = request.decode('utf-8')
+        request_line, request_rest = request.split('\r\n', 1)
+        request_header, request_body = request_rest.split('\r\n\r\n', 1)
+
+        # print("line------")
+        # print(request_line)
+        # print("header----")
+        # print(request_header)
+        # print("body------")
+        # print(request_body)
+
+        # self.line_analyse(request_line)
+        # self.header_analyse(request_header)
+
+    def line_analyse(self, request_line):
+        """请求行解析"""
         header_list = request_line.split(' ')
         self.method = header_list[0].upper()
         self.url = header_list[1]
@@ -23,7 +39,8 @@ class HttpHeaderAnalysis:
             self.url = '/index.html'
         self.protocol = header_list[2]
 
-    def passRequestHead(self, request_head):  # 定义head（字典）
+    def header_analyse(self, request_head):
+        """请求头解析"""
         head_options = request_head.split('\r\n')
         for option in head_options:
             key, val = option.split(': ', 1)
@@ -31,12 +48,4 @@ class HttpHeaderAnalysis:
         if 'Cookie' in self.head:
             self.Cookie = self.head['Cookie']  # 提取出来Cookie
 
-    def passRequest(self, request):  # 解析请求报文入口函数
-        request = request.decode('utf-8')
-        if len(request.split('\r\n', 1)) != 2:
-            return
-        request_line, body = request.split('\r\n', 1)
-        request_head = body.split('\r\n\r\n', 1)[0]
-        self.passRequestLine(request_line)
-        self.passRequestHead(request_head)
-        # *********************self填充完成***************************
+
