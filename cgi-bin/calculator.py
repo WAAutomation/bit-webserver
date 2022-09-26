1b47863f2915201286117806dbe21ff689b0076c
JiaweiYe <1241818274@qq.com>
2022-09-26 22:42:35 +0800

    add parameter handling

--- cgi-bin/calculator.py
+++ cgi-bin/calculator.py
@@ -2,8 +2,9 @@ import sys
 
 
 def cal(oprd1, oprd2, oprt):
-    oprd1 = float(oprd1)
-    oprd2 = float(oprd2)
+    oprd1 = float(oprd1.rstrip())
+    oprd2 = float(oprd2.rstrip())
+    oprt = oprt.rstrip()
     if oprt == '+':
         return oprd1 + oprd2
     elif oprt == '-':
@@ -15,5 +16,5 @@ def cal(oprd1, oprd2, oprt):
 
 
 if __name__ == "__main__":
-    c = cal(sys.argv[1], sys.argv[2])
-    # print(c)
+    answer = cal(sys.argv[1], sys.argv[2], sys.argv[3])
+    print(answer)
